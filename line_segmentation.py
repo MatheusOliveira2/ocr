89ff5c5551646797ef51ba947670180e9580369c
Matheus Oliveira <matheusalmeida.soliveira@gmail.com>
2021-09-27 21:15:49 -0300

    valley and peaks found

--- line_segmentation.py
+++ line_segmentation.py
@@ -2,6 +2,7 @@ import cv2
 from matplotlib import pyplot as plt
 import numpy as np
 import statistics
+from peakdetect import peakdetect
 
 def line_segmentation(image):
     height = image.shape[0]-200
@@ -25,6 +26,9 @@ def line_segmentation(image):
 
 def histogram(array):
     ax2 = plt.subplot(2, 1, 2)
+    peaks = peakdetect(array, lookahead=20)
+    higherPeaks = np.array(peaks[1])
+    ax2.plot(higherPeaks[:,0], higherPeaks[:,1], 'bx')
     ax2.bar(range(len(array)), array, color='r')
     # plt.show()
     plt.savefig("histogram-line5.png")
@@ -40,13 +44,20 @@ def find_line(image,array):
     
     width = image.shape[1]
     lines = []
-    for a in range(len(array)):
-        if array[a] <= mean and array[a] > mean/2:
-            lines.append(a)
+    # for a in range(len(array)):
+    #     if array[a] <= mean and array[a] > mean/2:
+    #         lines.append(a)
+
+    peaks = peakdetect(array, lookahead=20)
+    higherPeaks = np.array(peaks[1])
+
+    for peak in higherPeaks:
+            lines.append(peak)
 
     print ("lines: ", len(lines))
     
     imageColor = cv2.cvtColor(image, cv2.COLOR_GRAY2RGBA)
+
     
     for i in range(len(lines)):
         for j in range(width):
@@ -58,6 +69,10 @@ def find_line(image,array):
 
 
 image = cv2.imread('exemplo9.jpeg', 0)
+image = cv2.bitwise_not(image)
+kernel = np.ones((2,2), np.uint8)
+image = cv2.dilate(image, kernel, iterations=1)
+image = cv2.bitwise_not(image)
 line_segmentation(image)
 
     
